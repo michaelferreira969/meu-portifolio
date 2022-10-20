e10424126170e46170cbf22281638e1e5eface8e
michael-ferreira <93886512+michaelferreira969@users.noreply.github.com>
2022-10-20 16:19:18 -0300

    primeiro commit modo dark
    Primeira atualização com modo dark incluído

--- javascript/script.js
+++ javascript/script.js
@@ -31,23 +31,40 @@ function acessarUrl(url) {
     window.open(url, '_blank');
 }
 
+//Pegar elemento body
+const body = document.getElementById('body');
+
+//Pegar o nome da classe do elemento body
+let modo;
 
 /*Alterar modo de cor do site*/
 function alterarModo() {
-    //Pegar elemento body
-    const body = document.getElementById('body');
-
-    //Pegar o nome da classe do elemento body
-    let modo = body.className;
+    modo = body.className;
 
     //Se modo escuro alterar para claro
     if (modo === 'dark') {
         body.className = 'light';
+        modoLight()
     } else if (modo === 'light') {
         body.className = 'dark';
+        modoDark();
     }
+
+    console.log(modo);
 }
 
-function identificarModo() {
-    document.getElementById('body').className;
+/*Variáveis utilizadas nos modos*/
+let img = document.getElementById('img-capa');
+
+/*Função para alterar elementos da página*/
+function modoDark() {
+    console.log(modo);
+    /*Troca de imagem de capa*/
+    img.src = 'img/capa-michael-2.png';
+}
+
+function modoLight() {
+    console.log(modo);
+    /*Troca de imagem de capa*/
+    img.src = 'img/capa-michael.png';
 }
