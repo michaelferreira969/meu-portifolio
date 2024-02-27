5761a189eff32cd2b29c1c409c6a56793129db10
michael-ferreira <michaelferrys969@gmail.com>
2024-02-27 20:41:31 -0300

    Atualização

--- javascript/script.js
+++ javascript/script.js
@@ -1,70 +1,60 @@
-const btnMobile = document.getElementById('btn-mobile');
+class Controller {
+    constructor() {
+        const btnMobile = $('#btn-mobile');
+    }
 
-function toggleMenu(event) {
-    /*Para corrigir erro do touchstart de fechar e abrir*/
-    if (event.type === 'touchstart') event.preventDefault();
+    _events() {
+        const _ = this; 
 
-    /*Selecionar Nav*/
-    const nav = document.getElementById('nav');
+        $("#btn-mobile").on("click", function() { _._ToggleMenu($(this)); });
 
-    /*Adicione caso não tenha, remova caso tenha*/
-    nav.classList.toggle('active'); 
+        //Aciona mais rápido
+        $("#btn-mobile").on("touchstart", function() { _._ToggleMenu($(this)); });
+
+        $("#btn-link").on("click", function() { _._AcessarUrl($(this)); });
+    }
+
+    _ToggleMenu(event) {
+        /*Para corrigir erro do touchstart de fechar e abrir*/
+        if (event.type === 'touchstart') event.preventDefault();
     
-    /*Para os leitores de tela*/
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
-}
-
-/*Para que ao clicar acione*/
-btnMobile.addEventListener('click', toggleMenu);
-
-/*Para que ao clicar acione mais rápido*/
-btnMobile.addEventListener('touchstart', toggleMenu);
-
-/*Para acessar urls dos botões*/
-function acessarUrl(url) {
-    window.open(url, '_blank');
-}
-
-//Pegar elemento body
-const body = document.getElementById('body');
-
-//Pegar o nome da classe do elemento body
-let modo;
-
-/*Alterar modo de cor do site*/
-function alterarModo() {
-    modo = body.className;
-
-    //Se modo escuro alterar para claro
-    if (modo === 'dark') {
-        body.className = 'light';
-        modoLight()
-    } else if (modo === 'light') {
-        body.className = 'dark';
-        modoDark();
+        const nav = $('#nav');
+        
+        /*Adicione caso não tenha, remova caso tenha*/
+        nav.classList.toggle('active'); 
+        
+        /*Para os leitores de tela*/
+        const active = nav.classList.contains('active');
+        $(event.currentTarget).prop('aria-expanded', active);
+        if (active)
+            $(event.currentTarget).prop('aria-label', 'Fechar Menu');
+        else
+            $(event.currentTarget).prop('aria-label', 'Abrir Menu');
     }
 
-    console.log(modo);
-}
+    _AcessarUrl(url){
+        window.open(url, '_blank');
+    }
 
-/*Variáveis utilizadas nos modos*/
-let img = document.getElementById('img-capa');
+    _AlterarModo() {
+        //Pegar elemento body
+        const body = $('body');
 
-/*Função para alterar elementos da página*/
-function modoDark() {
-    console.log(modo);
-    /*Troca de imagem de capa*/
-    img.src = 'img/capa-michael-2.png';
-}
+        //Pegar o nome da classe do elemento body
+        let modo;
 
-function modoLight() {
-    console.log(modo);
-    /*Troca de imagem de capa*/
-    img.src = 'img/capa-michael.png';
-}
+        modo = body.className;
+
+        let img = $('#img-capa');
+
+        //Se modo escuro alterar para claro
+        if (modo === 'dark') {
+            body.className = 'light';
+            img.src = 'img/capa-michael.png';
+        } else if (modo === 'light') {
+            body.className = 'dark';
+            img.src = 'img/capa-michael-2.png';
+        }
+    }
+
+}(function () { new Controller() })
