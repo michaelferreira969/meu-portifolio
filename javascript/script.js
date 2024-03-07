56619bd0e94192d1acc24202315d59fc454c2181
michael-ferreira <michaelferrys969@gmail.com>
2024-03-07 19:20:05 -0300

    Atualização 2024

--- javascript/script.js
+++ javascript/script.js
@@ -1,43 +1,47 @@
 class Controller {
     constructor() {
         const btnMobile = $('#btn-mobile');
-
+        console.log("Teste")
         this._events();
     }
 
     _events() {
-        const _ = this; 
+        const _ = this;
 
-        $("#btn-mobile").on("click", function() { _._ToggleMenu($(this)); });
+        $("#btn-mobile").on("click", function () { _._ToggleMenu($(this)); });
 
         //Aciona mais rápido
-        $("#btn-mobile").on("touchstart", function() { _._ToggleMenu($(this)); });
-
-        $("#btn-link").on("click", function () { _._AcessarUrl($(this)); });
+        $("#btn-mobile").on("touchstart", function () { _._ToggleMenu($(this)); });
 
         $("#btn-m").on("click", function () { _._AlterarModo(); });
     }
 
+    _LerArquivo() {
+        fetch('arquivo.txt')
+            .then(response => response.text())
+            .then(text => {
+                const array = text.split("\n");
+                console.log(array);
+            })
+    }
+
+
     _ToggleMenu(event) {
-        /*Para corrigir erro do touchstart de fechar e abrir*/
+        //Para corrigir erro do touchstart de fechar e abrir
         if (event.type === 'touchstart') event.preventDefault();
-    
+
         const nav = $('#nav');
-        
-        /*Adicione caso não tenha, remova caso tenha*/
-        nav.classList.toggle('active'); 
-        
-        /*Para os leitores de tela*/
+
+        //Adicione caso não tenha, remova caso tenha
+        nav.classList.toggle('active');
+
+        //Para os leitores de tela
         const active = nav.classList.contains('active');
         $(event.currentTarget).prop('aria-expanded', active);
         if (active)
             $(event.currentTarget).prop('aria-label', 'Fechar Menu');
         else
             $(event.currentTarget).prop('aria-label', 'Abrir Menu');
-    }
-
-    _AcessarUrl(url){
-        window.open(url, '_blank');
     }
 
     _AlterarModo() {
@@ -60,5 +64,4 @@ class Controller {
             img.src = 'img/capa-michael-2.png';
         }
     }
-
-}(function () { new Controller() })
+} (function () { new Controller() })
